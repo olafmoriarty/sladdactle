8ccd6906ae7b877a352f4d36b491833718553cdd
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2022-10-13 16:19:40 +0200

    fix: Remove accent marks from guessed characters
    fixes #2

--- src/functions/washWord.js
+++ src/functions/washWord.js
@@ -1,5 +1,15 @@
 const washWord = word => {
-	return word.toLowerCase().trim();
+	let newWord = word.toLowerCase().trim();
+    newWord = newWord.replace(/[àáâã]/g,"a");
+    newWord = newWord.replace(/ç/g,"c");
+    newWord = newWord.replace(/[èéêë]/g,"e");
+    newWord = newWord.replace(/[ìíîï]/g,"i");
+    newWord = newWord.replace(/ñ/g,"n");                
+    newWord = newWord.replace(/[òóôõ]/g,"o");
+    newWord = newWord.replace(/œ/g,"oe");
+    newWord = newWord.replace(/[ùúûü]/g,"u");
+    newWord = newWord.replace(/[ýÿ]/g,"y");
+	return newWord;
 }
 
 export default washWord;
